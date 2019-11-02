c94a6278cb92b9d019330a2000498f99c3aabb49
undym <undym1@gmail.com>
2019-11-02 13:44:11 +0900

    verup

--- src/savedata.ts
+++ src/savedata.ts
@@ -14,7 +14,7 @@ import { PartySkill } from "./partyskill.js";
 
 
 export class Version{
-    static readonly NOW = new Version(0,21,0);
+    static readonly NOW = new Version(0,20,1);
 
     private values:number[];
 
