300792e8ceab42b8f5d941fc8dafc8c0a58421bc
undym <undym1@gmail.com>
2019-11-09 15:49:58 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -167,7 +167,7 @@ const setTitle = ()=>{
             for(const s of Version.updateInfo){
                 msg.push(s);
             }
-            msg.push("test10");
+            msg.push("test11");
             msg.push(`${window.location}`);
 
             msg.forEach((s,i)=>{
--- src/sound.ts
+++ src/sound.ts
@@ -5,53 +5,53 @@ export class Sound{
     private static _values:Sound[] = [];
     static get values():ReadonlyArray<Sound>{return this._values;}
 
-    // private ac:AudioContext;
-    // private src:AudioBufferSourceNode;
-    // private buffer:AudioBuffer;
-    private audio:HTMLAudioElement;
+    private ac:AudioContext;
+    private src:AudioBufferSourceNode;
+    private buffer:AudioBuffer;
+    // private audio:HTMLAudioElement;
 
     constructor(private path:string){
         Sound._values.push(this);
     }
     /**ブラウザの制限のため、TouchEventの中で初期化しなければならない。 */
     init(){
-        this.audio = new Audio(this.path);
-        this.audio.muted = true;
-        this.audio.play();
-        this.audio.pause();
-        this.audio.muted = false;
-        // this.ac = new AudioContext();
-        // const request = new XMLHttpRequest();
-        // request.onload = ()=>{
-        //     var audioData = request.response;
-        //     this.ac.decodeAudioData(audioData, buffer=>{
-        //         this.buffer = buffer;
-        //     },e=>{
-        //         Util.msg.set("err"+this.path);
-        //         return "Error with decoding audio data " + this.path;
-        //     });
-        // };
-        // request.open("GET", this.path, true);
-        // request.responseType = 'arraybuffer';
-        // request.send();
-        // this.audio.onload = ev=>{
-        //     Util.msg.set("load");
-        // };
-        this.audio.onloadedmetadata = ev=>{
-            Util.msg.set("medialoaded");
-        }
+        // this.audio = new Audio(this.path);
+        // this.audio.muted = true;
+        // this.audio.play();
+        // this.audio.pause();
+        // this.audio.muted = false;
+
+
+        this.ac = new AudioContext();
+        const request = new XMLHttpRequest();
+        request.onload = ()=>{
+            var audioData = request.response;
+            this.ac.decodeAudioData(audioData, buffer=>{
+                this.buffer = buffer;
+                Util.msg.set("buffer");
+            },e=>{
+                Util.msg.set("err"+this.path);
+                return "Error with decoding audio data " + this.path;
+            });
+        };
+        request.open("GET", window.location + this.path, true);
+        Util.msg.set(`${window.location}${this.path}`)
+        request.responseType = 'arraybuffer';
+        request.send();
     }
 
     play(){
-        this.audio.currentTime = 1;
-        this.audio.play();
-        // if(!this.buffer){return;}
+        // this.audio.currentTime = 0;
+        // this.audio.play();
 
-        // this.src = this.ac.createBufferSource();
-        // this.src.buffer = this.buffer;
-        // this.src.connect(this.ac.destination);
+
+        if(!this.buffer){return;}
+
+        this.src = this.ac.createBufferSource();
+        this.src.buffer = this.buffer;
+        this.src.connect(this.ac.destination);
         
-        // this.src.start(0);
+        this.src.start(0);
     }
 
     
